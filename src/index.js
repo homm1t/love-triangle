c4eb26abfd83592a04bba5fa99a30694bd4eb6cf
homm1t <ncksvtch@gmail.co>
2019-03-19 20:57:39 +0300

    add love triangle functionality

--- src/index.js
+++ src/index.js
@@ -2,6 +2,46 @@
  * @param preferences - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
  */
+
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  let loveRelations = [];
+  for (let i = 0; i < preferences.length; i++) {
+    const currentLove = preferences[i];
+    if (!loveRelations[i + 1]) {
+      loveRelations[i + 1] = [currentLove]
+    } else {
+      loveRelations[i + 1].push(currentLove)
+    }
+  }
+
+  let count = 0;
+  for (let i = 1; i < loveRelations.length; i++) {
+    const currentLove = loveRelations[i];
+    if (!currentLove) {
+      continue;
+    }
+    for (let x = 0; x < currentLove.length; x++) {
+      const next = currentLove[x];
+      if (!next) {
+        continue;
+      }
+      var nextLoves = loveRelations[next];
+      for (let z = 0; z < nextLoves.length; z++) {
+        const nextnext = nextLoves[z];
+        if (!nextnext || nextnext == next) {
+          continue;
+        }
+        let nextnextLoves = loveRelations[nextnext];
+        for (let m = 0; m < nextnextLoves.length; m++) {
+          if (nextnextLoves[m] == i && nextnextLoves[m] != nextnext) {
+            count++;
+            loveRelations[i].pop(x);
+            loveRelations[next].pop(z);
+            loveRelations[nextnext].pop(m);
+          }
+        }
+      }
+    }
+  }
+  return count;
 };
